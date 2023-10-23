cd5aa228b111794cfa7690acea55f4ceb1758f15
jadonus <jadongearhart@gmail.com>
2023-10-23 08:47:31 -0400

    admin shows url

--- src/routes/admin/[generatedLink]/+page.svelte
+++ src/routes/admin/[generatedLink]/+page.svelte
@@ -10,8 +10,6 @@
   let apistuff;
   let dateData = []; // An array to store the date data for the pie chart
   let pieChart;
-
-  // Import Chart.js
   import Chart from "chart.js/auto";
 
   function formatDate(isoDate) {
@@ -33,10 +31,10 @@
     pieChart.chart = new Chart(ctx, {
       type: "pie",
       data: {
-        labels: dateData.map((date) => formatDate(date)),
+        labels: apistuff.data.map((item) => formatDate(item.Date)), // Use names as labels
         datasets: [
           {
-            data: dateData.length > 0 ? Array(dateData.length).fill(1) : [],
+            data: apistuff.data.map((item) => 1), // Set all data points to 1
             backgroundColor: colors,
             borderWidth: 0, // Set borderWidth to 0 to remove the border
           },
@@ -48,9 +46,9 @@
       },
     });
   }
-
   // Create the initial empty chart
   onMount(() => {
+    console.log(dateData);
     const ctx = pieChart.getContext("2d");
     pieChart.chart = new Chart(ctx, {
       type: "pie",
@@ -68,8 +66,6 @@
         maintainAspectRatio: false,
       },
     });
-
-    console.log(dateData);
   });
 
   onMount(async () => {
@@ -110,8 +106,10 @@
 
     {#if apistuff && Array.isArray(apistuff.data) && apistuff.data.length > 0}
       <p>{apistuff.data.length} Response(s)</p>
+      <a href={genlink}>Url Link </a>
     {:else}
       <p>No Responses!</p>
+      <a href={genlink}>Url Link </a>
     {/if}
     <div class="pie-chart">
       <canvas bind:this={pieChart} />
